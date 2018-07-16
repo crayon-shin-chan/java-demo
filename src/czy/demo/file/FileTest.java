2ae7eceeec2b750b2720b3eee44b5521458f64fc
陈振远 <1253665625@qq.com>
2018-07-16 14:21:00 +0800

    修改文件测试

--- src/czy/demo/file/FileTest.java
+++ src/czy/demo/file/FileTest.java
@@ -3,6 +3,7 @@ package czy.demo.file;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Date;
 
 public class FileTest {
 
@@ -19,8 +20,22 @@ public class FileTest {
         say("文件路径为："+file.getPath());
         say("文件绝对路径为："+file.getAbsolutePath());
         say("文件父路径为："+file.getParent());
+
+        /* 文件不存在时，长度为0，不可写，不可读，不可执行 */
+        say("文件长度为："+file.length());
+        say("文件是否存在："+file.exists());
+        say("文件是否是目录："+file.isDirectory());
+        say("文件是否是普通文件："+file.isFile());
+        say("文件路径是否是绝对的："+file.isAbsolute());
+        say("文件是否隐藏："+file.isHidden());
+        say("文件最后修改时间为："+new Date(file.lastModified()).toString());
+        say("文件是否可执行："+file.canExecute());
+        say("文件是否可读："+file.canRead());
+        say("文件是否可写："+file.canWrite());
         say("文件转为字符串："+file.toString());
 
+        /* 文件不存在时，创建新文件成功 */
+        say("创建新文件是否成功："+file.createNewFile());
 
         /* 创建File对象不会生成文件，只有打开输出流时才会生成文件，不管是否写出数据 */
         OutputStream out = new FileOutputStream(file);
