17d0b790281830d9c0bfe0d72879e12a54a4f606
陈振远 <1253665625@qq.com>
2018-07-16 14:38:03 +0800

    添加SHA256 SHA384 SHA512的RSA签名、验签测试

--- src/czy/demo/crypto/rsa/RSATest.java
+++ src/czy/demo/crypto/rsa/RSATest.java
@@ -14,6 +14,10 @@ public class RSATest {
 
     private static PublicKey public_key;
 
+    private static Base64.Encoder encoder = Base64.getEncoder();
+
+    private static Base64.Decoder decoder = Base64.getDecoder();
+
     static{
         try{
             /* 密钥对生成器 */
@@ -26,6 +30,11 @@ public class RSATest {
             public_key = keyPair.getPublic();
             /* 生成的私钥 */
             private_key = keyPair.getPrivate();
+
+            say("生成的公钥为："+encoder.encodeToString(public_key.getEncoded()));
+
+            say("生成的私钥为："+encoder.encodeToString(private_key.getEncoded()));
+
         }catch(Exception ex){
 
         }
@@ -35,9 +44,25 @@ public class RSATest {
 
     public static void main(String[] args)throws Exception{
 
-        String sign = md5Sign(content);
-        say("签名字符串为："+sign);
-        say("验签结果为："+md5Verify(sign,content));
+        String md5Sign = md5Sign(content);
+        say("md5签名字符串为："+md5Sign);
+        say("md5验签结果为："+md5Verify(md5Sign,content));
+
+        String sha1Sign = sha1Sign(content);
+        say("sha1签名字符串为："+sha1Sign);
+        say("sha1验签结果为："+sha1Verify(sha1Sign,content));
+
+        String sha256Sign = sha256Sign(content);
+        say("sha256签名字符串为："+sha256Sign);
+        say("sha256验签结果为："+sha256Verify(sha256Sign,content));
+
+        String sha384Sign = sha384Sign(content);
+        say("sha384签名字符串为："+sha384Sign);
+        say("sha384验签结果为："+sha384Verify(sha384Sign,content));
+
+        String sha512Sign = sha512Sign(content);
+        say("sha512签名字符串为："+sha512Sign);
+        say("sha512验签结果为："+sha512Verify(sha512Sign,content));
     }
 
     public static void say(Object o){
@@ -48,13 +73,69 @@ public class RSATest {
         Signature signature = Signature.getInstance("MD5withRSA");
         signature.initSign(private_key);
         signature.update(data);
-        return Base64.getEncoder().encodeToString(signature.sign());
+        return encoder.encodeToString(signature.sign());
     }
 
     public static boolean md5Verify(String s,byte[] data)throws Exception{
         Signature signature = Signature.getInstance("MD5withRSA");
         signature.initVerify(public_key);
         signature.update(data);
-        return signature.verify(Base64.getDecoder().decode(s));
+        return signature.verify(decoder.decode(s));
+    }
+
+    public static String sha1Sign(byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA1withRSA");
+        signature.initSign(private_key);
+        signature.update(data);
+        return encoder.encodeToString(signature.sign());
+    }
+
+    public static boolean sha1Verify(String s,byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA1withRSA");
+        signature.initVerify(public_key);
+        signature.update(data);
+        return signature.verify(decoder.decode(s));
+    }
+
+    public static String sha256Sign(byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(private_key);
+        signature.update(data);
+        return encoder.encodeToString(signature.sign());
+    }
+
+    public static boolean sha256Verify(String s,byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initVerify(public_key);
+        signature.update(data);
+        return signature.verify(decoder.decode(s));
+    }
+
+    public static String sha384Sign(byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA384withRSA");
+        signature.initSign(private_key);
+        signature.update(data);
+        return encoder.encodeToString(signature.sign());
+    }
+
+    public static boolean sha384Verify(String s,byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA384withRSA");
+        signature.initVerify(public_key);
+        signature.update(data);
+        return signature.verify(decoder.decode(s));
+    }
+
+    public static String sha512Sign(byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA512withRSA");
+        signature.initSign(private_key);
+        signature.update(data);
+        return encoder.encodeToString(signature.sign());
+    }
+
+    public static boolean sha512Verify(String s,byte[] data)throws Exception{
+        Signature signature = Signature.getInstance("SHA512withRSA");
+        signature.initVerify(public_key);
+        signature.update(data);
+        return signature.verify(decoder.decode(s));
     }
 }
