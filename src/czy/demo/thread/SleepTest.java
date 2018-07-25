68844563a0eb6612b0b4960ac1a6380dca9eaa77
陈振远 <1253665625@qq.com>
2018-07-25 10:14:01 +0800

    添加线程沉睡测试，线程沉睡实际时间与预计时间差不多，有时会有毫秒级别差别

--- src/czy/demo/thread/SleepTest.java
+++ src/czy/demo/thread/SleepTest.java
@@ -1,24 +1,23 @@
 package czy.demo.thread;
 
 import java.util.Date;
+import java.util.Random;
 
 public class SleepTest {
 
     public static void main(String[] args)throws Exception{
-        Thread t = new Thread(()->{
-            while(true){
-                System.out.println("当前时间为："+new Date().toString());
-                try{
-                    /* 可以使当前线程沉睡3秒，每次沉睡之后会从沉睡处继续运行 */
-                    System.out.println("线程开始沉睡");
-                    Thread.sleep(3000);
-                    System.out.println("线程已经睡醒");
-                }catch(Exception ex){
+        Random random = new Random(100);
+        while(true){
+            Date start = new Date();
+            System.out.println("线程开始沉睡："+start.toString());
+            int duration = random.nextInt(5000);
+            Thread.sleep(duration);
+            Date end = new Date();
+            System.out.println("线程沉睡结束："+end.toString());
+            System.out.println("应该沉睡时间："+duration);
+            System.out.println("实际沉睡时间："+(end.getTime()-start.getTime()));
 
-                }
-            }
-        });
-        t.start();
+        }
     }
 
 }
