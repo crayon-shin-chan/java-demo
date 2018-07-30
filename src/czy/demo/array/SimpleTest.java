d1170233ae176c1f8c43102bc8f81a1968428a9c
陈振远 <1253665625@qq.com>
2018-07-30 18:04:27 +0800

    添加简单数组测试

---
+++ src/czy/demo/array/SimpleTest.java
@@ -0,0 +1,22 @@
+package czy.demo.array;
+
+public class SimpleTest {
+
+    public static void main(String[] args){
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        say("数组长度为："+arr.length);
+
+        /* 数组转换为字符串并不是实际内容 */
+        say("数组转换为字符串为："+arr.toString());
+
+        /* 克隆数组与原数组引用不同了 */
+        say("克隆数组与原数组比较引用为；"+(arr==arr.clone()));
+    }
+
+    public static void say(Object o){
+        System.out.println(o);
+    }
+
+}
