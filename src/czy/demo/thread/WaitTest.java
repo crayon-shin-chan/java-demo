52cf57b133023e16b7e8d11d6b4f9d51de97e73a
陈振远 <1253665625@qq.com>
2018-07-30 10:17:22 +0800

    添加逻辑运算短路测试

--- src/czy/demo/thread/WaitTest.java
+++ src/czy/demo/thread/WaitTest.java
@@ -1,58 +1,39 @@
 package czy.demo.thread;
 
-import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class WaitTest {
 
-    public static Integer num = 0 ;
-    public static Random ran = new Random();
+    public static AtomicInteger num = new AtomicInteger(0) ;
 
-    /* 这个例子写的不对，后面还要重写 */
+    /**
+     * 这里终于成功，最大的问题是wait/notify这两个方法都必须在获取锁之后进行调用
+     * 如果无锁调用，会抛出异常
+     * wait操作相当于把同步调用分割开来，其实会破坏同步的本来意义
+     * 因为在wait时间内，其他获取锁的线程，会对对象进行操作
+     * 这里会有一个问题：
+     * 首先，简单对象不能加锁，所以需要使用包装类型
+     * 但是，在执行Integer = 1 + 1这种操作时，都会创建一个新的包装器对象，而不是使用原有的包装器
+     * 这样锁对象就被替换了，而我们持有的是操作之前的对象的锁，所以调用wait就会发生异常
+     * 这里需要使用java.util.concurrent.atomic.Atomic开头的包装器对象，这些对象直接操作内存，不会改变对象引用
+     */
     public static void main(String[] args)throws Exception{
-        new Thread(new Runnable() {
-            @Override
-            public void run(){
-                while(true){
-                    try{
-                        synchronized (num){
-                            System.out.println("线程一获取锁");
-                            System.out.println("当前值为："+num);
-                            Thread.currentThread().sleep(3000);
-                            if(num>=0){
-                                /* wait方法只是一个短暂的等待，即给其他线程一个获取锁的机会 */
-                                num.notifyAll();
-                                num.wait();
-                            }else {
-                                num += ran.nextInt(5);
-                            }
-                        }
-                    }catch(Exception ex){}
-
+        new Thread(()->{
+            try{
+                synchronized (num){
+                    System.out.println("线程即将进入等待："+num.getAndAdd(1));
+                    num.wait();
+                    System.out.println("线程等待结束:"+num.getAndAdd(1));
                 }
+            }catch(Exception ex){
+                System.out.println("线程内发生异常"+ex);
             }
         }).start();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run(){
-                while(true){
-                    try{
-                        synchronized (num){
-                            System.out.println("线程二获取锁");
-                            System.out.println("当前值为："+num);
-                            Thread.currentThread().sleep(3000);
-                            if(num>=0){
-                                num-=ran.nextInt(5);
-                            }else{
-                                num.notifyAll();
-                                num.wait();
-                            }
-
-                        }
-                    }catch(Exception ex){}
-
-                }
-            }
-        }).start();
+        Thread.sleep(3000);
+        synchronized (num){
+            System.out.println("准备通知锁上的等待对象："+num.getAndAdd(1));
+            num.notifyAll();
+            System.out.println("通知结束："+num.getAndAdd(1));
+        }
     }
 }
