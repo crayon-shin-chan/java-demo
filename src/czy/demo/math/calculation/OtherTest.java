b7b52d6cf254532f7d8ac41b7155bc575f4c790d
陈振远 <1253665625@qq.com>
2018-07-30 17:43:15 +0800

    添加其他计算测试

---
+++ src/czy/demo/math/calculation/OtherTest.java
@@ -0,0 +1,18 @@
+package czy.demo.math.calculation;
+
+public class OtherTest {
+
+    public static void main(String[] args){
+
+        System.out.println("-1的绝对值为："+Math.abs(-1));
+        System.out.println("-1.1的绝对值为："+Math.abs(-1.1));
+        System.out.println("1的绝对值为："+Math.abs(1));
+        System.out.println("1.1的绝对值为："+Math.abs(1.1));
+        System.out.println();
+
+        System.out.println("获取1与5的最大值："+Math.max(1,5));
+        System.out.println("获取1与5的最小值："+Math.min(1,5));
+        System.out.println();
+    }
+
+}
