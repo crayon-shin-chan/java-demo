a715ce6624bbc4101de078584d9f0db888a8ba66
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-10-15 10:48:38 +0800

    *示例

--- src/czy/demo/grammar/innerclass/local/Test.java
+++ src/czy/demo/grammar/innerclass/local/Test.java
@@ -2,7 +2,6 @@ package czy.demo.grammar.innerclass.local;
 
 public class Test {
 
-
     public static void main(String[] args){
         User user = new User();
         Object card = user.getCard();
--- src/czy/demo/runtime/CpuTest.java
+++ src/czy/demo/runtime/CpuTest.java
@@ -3,7 +3,6 @@ package czy.demo.runtime;
 public class CpuTest {
 
     public static void main(String[] args){
-
         /* 获取当前可用的cpu数量 */
         System.out.println("当前可用的cpu数量为："+Runtime.getRuntime().availableProcessors());
     }
