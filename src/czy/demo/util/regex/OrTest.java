27afba4d86c5c114247869f13305cd5704d957a2
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-10-15 11:27:23 +0800

    添加OR示例

--- src/czy/demo/util/regex/OrTest.java
+++ src/czy/demo/util/regex/OrTest.java
@@ -3,16 +3,15 @@ package czy.demo.util.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/* 捕获组示例 */
-public class CaptureTest {
+/* 元字符|示例 */
+public class OrTest {
 
     public static void main(String[] args){
 
-        String content = " 12  34 ";
+        String content = " 12 34 ";
 
-        /* 捕获组可以互相嵌套，大的首先遇到的捕获组在前面 */
-        /* ?:代表非捕获匹配，其中内容不会成为捕获组 */
-        String regex = "^ ((\\d*)(?:\\s*))(\\d*)(\\s*)$";
+        /* |可以起到或的作用(12|34)代表12或者34，而不是124或者134 */
+        String regex = "^ (12|34) (12|34) $";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(content);
 
