93b5fe2a65b795e4b2ecb50efdeae3f8632f2d23
chenzhenyuan <1253665625@qq.com>
2018-12-31 21:25:12 +0800

    SleepTest

--- src/czy/demo/thread/status/SleepTest.java
+++ src/czy/demo/thread/status/SleepTest.java
@@ -1,23 +1,25 @@
 package czy.demo.thread.status;
 
-import java.util.Date;
-import java.util.Random;
 
 public class SleepTest {
 
-    public static void main(String[] args)throws Exception{
-        Random random = new Random(100);
-        while(true){
-            Date start = new Date();
-            System.out.println("线程开始沉睡："+start.toString());
-            int duration = random.nextInt(5000);
-            Thread.sleep(duration);
-            Date end = new Date();
-            System.out.println("线程沉睡结束："+end.toString());
-            System.out.println("应该沉睡时间："+duration);
-            System.out.println("实际沉睡时间："+(end.getTime()-start.getTime()));
-
+    private static final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            for(int i=0;i<100;i++){
+                System.out.println("正在执行！");
+                /* 线程沉睡，可能抛出Interrupt异常 */
+                try {
+                    Thread.sleep(1000);
+                }catch (InterruptedException ex){
+                    System.err.println("发生了异常");
+                }
+            }
         }
+    };
+
+    public static void main(String[] args){
+       new Thread(runnable).start();
     }
 
 }
