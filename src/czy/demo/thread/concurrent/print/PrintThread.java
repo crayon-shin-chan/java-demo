247b7f6a7e8676f4a199102fcfa3c9c3defebd09
chenzhenyuan <1253665625@qq.com>
2018-12-31 21:04:26 +0800

    PrintThread

---
+++ src/czy/demo/thread/concurrent/print/PrintThread.java
@@ -0,0 +1,18 @@
+package czy.demo.thread.concurrent.print;
+
+public class PrintThread extends Thread {
+
+    private String message;
+
+    public PrintThread(String message){
+        super();
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0;i<10000;i++){
+            System.out.println(message);
+        }
+    }
+}
