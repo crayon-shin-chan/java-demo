89290fab73eea77cf9b8d556767400433c161b7d
chenzhenyuan@skyable.cn <1253665625@qq.com>
2019-01-16 15:37:02 +0800

    Decimal格式化测试

---
+++ src/czy/demo/util/number/DecimalFormatTest.java
@@ -0,0 +1,18 @@
+package czy.demo.util.number;
+
+import java.text.DecimalFormat;
+import java.util.logging.Logger;
+
+public class DecimalFormatTest {
+
+    private static final Logger logger = Logger.getLogger(DecimalFormatTest.class.getName());
+
+    public static void main(String[] args){
+        /* 可以将数字格式化为指定位数 */
+        DecimalFormat format = new DecimalFormat("#.00");
+        logger.info(format.format(12));
+        logger.info(format.format(12.1));
+        logger.info(format.format(12.1213));
+    }
+
+}
