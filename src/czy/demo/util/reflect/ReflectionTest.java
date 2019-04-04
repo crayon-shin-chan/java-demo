f6358471da8bfd31dd1afb08566993053c36006a
陈振远 <1253665625@qq.com>
2019-04-04 15:18:06 +0800

    unsafe操作

--- src/czy/demo/util/reflect/ReflectionTest.java
+++ src/czy/demo/util/reflect/ReflectionTest.java
@@ -1,17 +1,21 @@
 package czy.demo.util.reflect;
 
+import sun.reflect.CallerSensitive;
 import sun.reflect.Reflection;
 
-/* Reflection是sun提供的反射工具 */
+/* Reflection是sun提供的反射工具，貌似获取调用者操作只能在引导类加载器和扩展类加载器加载代码中进行 */
 public class ReflectionTest {
 
+    @CallerSensitive
     public static void main(String[] args){
         new Caller().call();
     }
 
     /* 调用者 */
+
     private static class Caller{
 
+        @CallerSensitive
         public void call(){
             new Callee().call();
         }
@@ -22,6 +26,7 @@ public class ReflectionTest {
     /* 被调用者 */
     private static class Callee{
 
+        @CallerSensitive
         public void call(){
             Class caller = Reflection.getCallerClass();
             System.out.println("调用者类名为："+caller.getName());
---
+++ src/czy/demo/util/unsafe/ArrayTest.java
@@ -0,0 +1,44 @@
+package czy.demo.util.unsafe;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/* unsafe是直接对内存操作的类，性能好，危险性大 */
+public class ArrayTest {
+
+
+    private static Unsafe U;
+
+    static {
+        try {
+            Field f= Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            U = (Unsafe) f.get(null);
+        }catch (Exception ex){
+
+        }
+    }
+
+    public static void main(String[] args)throws Exception{
+
+        int[] arr = new int[]{12,54,23,7};
+        printArray(arr);
+        /* 获取并且设置指定字节偏移处元素 */
+        U.getAndSetInt(arr,16L,1000);
+        printArray(arr);
+
+    }
+
+    /*
+     * 打印出数组元素
+     */
+    private static void printArray(int[] arr){
+        /* 第一个元素的偏移为16字节，不清楚为什么，每隔四个字节为一个int */
+        for(int i=16;i<32;i+=4){
+            System.out.println("数组偏移"+i+"处为："+U.getInt(arr,(long)i));
+        }
+    }
+
+
+}
