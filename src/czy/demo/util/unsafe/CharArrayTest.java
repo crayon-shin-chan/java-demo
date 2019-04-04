cae14bf26d74e8eae855da4bc7e2550889437934
陈振远 <1253665625@qq.com>
2019-04-04 15:49:56 +0800

    字符数组测试

--- src/czy/demo/util/unsafe/CharArrayTest.java
+++ src/czy/demo/util/unsafe/CharArrayTest.java
@@ -4,8 +4,9 @@ import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 
-/* unsafe是直接对内存操作的类，性能好，危险性大 */
-public class IntArrayTest {
+/* 字符数组测试 */
+public class CharArrayTest {
+
 
     private static Unsafe U;
 
@@ -22,25 +23,26 @@ public class IntArrayTest {
 
     public static void main(String[] args){
 
-        int[] arr = new int[]{12,54,23,7,89};
+        char[] arr = new char[]{'A','B','C','D','E','F','G','H'};
         /* 数组类型都有一个基本偏移，表示最开始的元素存储于基本偏移字节处 */
         System.out.println("int数组基本偏移为："+U.arrayBaseOffset(arr.getClass()));
         /* 下标精度代表了每个元素间隔的字节数量 */
         System.out.println("数组类下标精度为："+U.arrayIndexScale(arr.getClass()));
         printArray(arr);
         /* 获取并且设置指定字节偏移处元素 */
-        U.getAndSetInt(arr,16L,1000);
+        U.putChar(arr,16L,'Z');
         printArray(arr);
     }
 
     /*
      * 打印出数组元素
      */
-    private static void printArray(int[] arr){
+    private static void printArray(char[] arr){
         /* 第一个元素的偏移为16字节，为int数组的基本偏移，每隔四个字节为一个int */
         for(int i=0;i<arr.length;i++){
             long offset = U.arrayBaseOffset(arr.getClass()) + i*U.arrayIndexScale(arr.getClass());
-            System.out.println("数组偏移"+offset+"处为："+U.getInt(arr,offset));
+            System.out.println("数组偏移"+offset+"处为："+U.getChar(arr,offset));
         }
     }
+
 }
--- src/czy/demo/util/unsafe/LongArrayTest.java
+++ src/czy/demo/util/unsafe/LongArrayTest.java
@@ -4,8 +4,9 @@ import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 
-/* unsafe是直接对内存操作的类，性能好，危险性大 */
-public class IntArrayTest {
+/* long数组测试 */
+public class LongArrayTest {
+
 
     private static Unsafe U;
 
@@ -22,25 +23,26 @@ public class IntArrayTest {
 
     public static void main(String[] args){
 
-        int[] arr = new int[]{12,54,23,7,89};
-        /* 数组类型都有一个基本偏移，表示最开始的元素存储于基本偏移字节处 */
-        System.out.println("int数组基本偏移为："+U.arrayBaseOffset(arr.getClass()));
-        /* 下标精度代表了每个元素间隔的字节数量 */
+        long[] arr = new long[]{12,54,23,7,89};
+        /* 数组类型都有一个基本偏移，表示最开始的元素存储于基本偏移字节处，基本都是16 */
+        System.out.println("long数组基本偏移为："+U.arrayBaseOffset(arr.getClass()));
+        /* 下标精度代表了每个元素间隔的字节数量，为元素的存储字节数 */
         System.out.println("数组类下标精度为："+U.arrayIndexScale(arr.getClass()));
         printArray(arr);
         /* 获取并且设置指定字节偏移处元素 */
-        U.getAndSetInt(arr,16L,1000);
+        U.getAndSetLong(arr,16L,1000L);
         printArray(arr);
     }
 
     /*
      * 打印出数组元素
      */
-    private static void printArray(int[] arr){
+    private static void printArray(long[] arr){
         /* 第一个元素的偏移为16字节，为int数组的基本偏移，每隔四个字节为一个int */
         for(int i=0;i<arr.length;i++){
             long offset = U.arrayBaseOffset(arr.getClass()) + i*U.arrayIndexScale(arr.getClass());
-            System.out.println("数组偏移"+offset+"处为："+U.getInt(arr,offset));
+            System.out.println("数组偏移"+offset+"处为："+U.getLong(arr,offset));
         }
     }
+
 }
