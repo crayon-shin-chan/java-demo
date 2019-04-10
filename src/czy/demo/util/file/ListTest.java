846ea62776d6ae2d547de8d6764a5cab6729feff
陈振远 <1253665625@qq.com>
2019-04-10 16:12:07 +0800

    文件列表测试

--- src/czy/demo/util/file/ListTest.java
+++ src/czy/demo/util/file/ListTest.java
@@ -3,9 +3,12 @@ package czy.demo.util.file;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /* 目录列表测试 */
 public class ListTest {
+
+    private static int num = 0;
 
     public static void main(String[] args)throws Exception{
         Path path = Paths.get("src");
@@ -13,7 +16,10 @@ public class ListTest {
     }
 
     private static void print(Path src)throws Exception{
-        System.out.println("文件路径："+src.toFile().getAbsolutePath());
+
+        System.out.println("文件路径："+src.toFile().getAbsolutePath()+"，数量："+(num++));
+
+        /* 这个遍历不是多线程的，而是顺序的 */
         Files.list(src).forEach(path->{
             try {
                 print(path);
