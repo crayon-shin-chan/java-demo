177ef51197ad91e0a367270b66a0d5abadcaa71f
chenzhenyuan <1253665625@qq.com>
2019-10-14 17:01:02 +0800

    添加注释

--- src/czy/demo/net/udp/UDPClient.java
+++ src/czy/demo/net/udp/UDPClient.java
@@ -12,6 +12,7 @@ public class UDPClient {
         int i=1;
         while(i<=10){
             String data = "这是第"+i+"次发送";
+            /** 设置发送数据，长度，域名，端口 */
             DatagramPacket packet = new DatagramPacket(data.getBytes(), data.getBytes().length, InetAddress.getLocalHost() , 9000);
             datagramSocket.send(packet);
             i++;
